1f555761effdf694acbabd106ad35daa06f592fe
Raghu Betina <raghu@firstdraft.com>
2016-11-01 04:18:30 +0000

    Whitelist columns in LifeMeasure for ActiveAdmin

--- app/admin/life_measure.rb
+++ app/admin/life_measure.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register LifeMeasure do
 
+ permit_params :goal_name, :target_frequency, :time_span, :start_date, :user_id, :times_completed, :category_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
